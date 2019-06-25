1f45e0c5d3ad0a8f091ad62389f9b9fa0258799a
Pooja Bhaumik <pooja.b@bounceshare.com>
2019-06-25 23:46:29 +0530

    api_services to fetch list<events>

--- lib/ui/events_list_screen.dart
+++ lib/ui/events_list_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_events/api_services.dart';
+import 'package:flutter_events/model/events_model.dart';
 
 
 class EventsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(),
+      body: Container(
+        child: FutureBuilder<List<Events>>(
+          future: getEventsList(),
+          builder: (context, snapshot){
+            return Text('${snapshot.data[0].address}');
+          },
+        ),
+      ),
     );
   }
 }
