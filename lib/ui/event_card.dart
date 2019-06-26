c51a48a3b7523a3857806bcf84564718043b31bf
Pooja Bhaumik <pooja.b@bounceshare.com>
2019-06-26 22:48:40 +0530

    Added fav button to event_card

--- lib/ui/event_card.dart
+++ lib/ui/event_card.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_events/model/events_model.dart';
 
-class EventCard extends StatelessWidget {
+class EventCard extends StatefulWidget {
 
   final Event event;
   EventCard({this.event});
 
   @override
+  _EventCardState createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+
+  var favouriteButton = IconButton(
+    onPressed: (){},
+    icon: Icon(Icons.favorite_border),
+  );
+
+  @override
   Widget build(BuildContext context) {
-    print('event : ${event.city}');
+    print('event : ${widget.event.city}');
     return Container(
       margin: EdgeInsets.all(10.0),
       child: Card(child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text('${event.city}'),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(widget.event.eventName),
+                  Text(widget.event.city)
+                ],
+              ),
+            ),
+            favouriteButton,
+          ],
+        )
       )),
     );
   }
