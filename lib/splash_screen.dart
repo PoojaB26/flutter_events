ac86e43a6823921fbaea76d47a1d8938feb75f58
Pooja Bhaumik <pooja.b@bounceshare.com>
2019-06-30 02:05:28 +0530

    section1: splash screen + timer

---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_events/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadTimer();
+
+  }
+
+  Future<Timer> loadTimer() async {
+    return new Timer(Duration(seconds: 8), navigate);
+  }
+
+  navigate() async {
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(20),
+        child: Image.network('https://itsallwidgets.com/images/logo.png'),
+      ),
+    );
+  }
+
+}
